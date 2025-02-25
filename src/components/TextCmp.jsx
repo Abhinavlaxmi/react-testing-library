28662d54c2971312450632ec406830da703afccc
Abhinavlaxmi <jaiswalabhinav12@gmail.com>
2025-02-25 13:16:37 +0530

    wrote test cases for Text component

---
+++ src/components/TextCmp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TextCmp = () => {
+  return (
+    <div className="App">
+        <p>First React Testing App.</p>
+    </div>
+  );
+};
+
+export default TextCmp;
---
+++ src/tests/TextCpm.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import TextCmp from "../components/TextCmp";
+
+test("Testing text of component", ()=>{
+    render(<TextCmp />)
+    // const textItem = screen.getByText("First React Test App.")      //case sensitive
+    const textItem = screen.getByText(/first react testing app./i)
+    expect(textItem).toBeInTheDocument();
+})
